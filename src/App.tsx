576ebf81f5aeb12e63f998883e86c6d72a434d54
johnny-netdev <johnny.r.halliv@gmail.com>
2025-12-05 11:39:19 -0500

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+type Todo = Schema["Todo"]["type"] & {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
