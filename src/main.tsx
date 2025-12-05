bc20eef78e2dbbfe07886838d45f82b28d2820f2
johnny-netdev <johnny.r.halliv@gmail.com>
2025-12-05 13:11:27 -0500

    login button

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css"; 
 
 
@@ -13,8 +12,6 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
       <App />
-    </Authenticator>
   </React.StrictMode>
 );
